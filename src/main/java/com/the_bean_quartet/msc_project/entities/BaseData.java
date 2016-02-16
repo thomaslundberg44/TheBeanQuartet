f8c1acf4a4ab56b9696001809171c1fab6a24d47
thomaslundberg44 <tlundberg44@gmail.com>
2016-02-16 16:38:52 +0000

    Entities

---
+++ src/main/java/com/the_bean_quartet/msc_project/entities/BaseData.java
@@ -0,0 +1,88 @@
+package com.the_bean_quartet.msc_project.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+@NamedQueries({
+		@NamedQuery(name = "basedata.getAll", query = "select cd from Base_Data data where data.event_id > :event_id") })
+
+@Entity
+@Table(name = "Base_Data")
+public class BaseData implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private int id;
+
+//	@Column(name = "date")
+//	private String date;
+	@Column(name = "eventId")
+	private Integer eventId;
+	@Column(name = "failureClass")
+	private Integer failureClass;
+	@Column(name = "UEType")
+	private Integer UEType;
+	
+	public BaseData() {}
+	
+	public BaseData(Integer eventId, Integer failureClass, Integer UEType) {
+		//this.date = date;
+		this.eventId = eventId;
+		this.failureClass = failureClass;
+		this.UEType = UEType;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+//	public String getDate() {
+//		return date;
+//	}
+//
+//	public void setDate(String date) {
+//		this.date = date;
+//	}
+
+	public Integer getEventId() {
+		return eventId;
+	}
+
+	public void setEventId(Integer eventId) {
+		this.eventId = eventId;
+	}
+
+	public Integer getFailureClass() {
+		return failureClass;
+	}
+
+	public void setFailureClass(Integer failureClass) {
+		this.failureClass = failureClass;
+	}
+
+	public Integer getUEType() {
+		return UEType;
+	}
+
+	public void setUEType(Integer uEType) {
+		UEType = uEType;
+	}
+	
+	// might need relationship to failure class etc
+
+}
---
+++ src/main/java/com/the_bean_quartet/msc_project/entities/BaseDataList.java
@@ -0,0 +1,23 @@
+package com.the_bean_quartet.msc_project.entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class BaseDataList implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private Collection<BaseData> data;
+	
+	public Collection<BaseData> getBaseDataCollection() {
+		return data;
+	}
+	
+	public void setBaseDataCollection(Collection<BaseData> data) {
+		this.data = data;
+	}
+	
+}
