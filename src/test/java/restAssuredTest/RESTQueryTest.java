1736f48deb9f20f462cf6bbf438406f8b80e7330
Kang Yang <yang.d.kang@gmail.com>
2016-04-04 20:11:57 +0100

    <rest>

--- src/test/java/restAssuredTest/RESTQueryTest.java
+++ src/test/java/restAssuredTest/RESTQueryTest.java
@@ -3,67 +3,73 @@ package restAssuredTest;
 
 
 import static com.jayway.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
 
 import org.junit.Test;
 
 import com.jayway.restassured.RestAssured;
-import com.jayway.restassured.authentication.FormAuthConfig;
-import com.jayway.restassured.http.ContentType;
-import com.jayway.restassured.response.Response;
-import com.the_bean_quartet.msc_project.entities.Options;
-import com.the_bean_quartet.msc_project.entities.SysUser;
 
 public class RESTQueryTest {
 	
+//
+//    @Test
+//	public void resteBasedataImsirelationRestAssuredTest()
+//	{
+//   	System.out.println("1111111111111111111111111");
+//
+//	//String myJson = "\"344930000000011\"";
+//	String myJson = "\"344930000000011\"";
+//
+//	RestAssured.baseURI  = "http://localhost:8080/TheBeanQuartet/rest/basedata/imsirelation";	
+//
+//	Response r = 
+//			given()
+//	.contentType("application/json").
+//	body(myJson).
+//    when().
+//    post("");
+//    //.then().statusCode(200);
+//	
+//	System.out.println("2222222222222222222222");
+//	String body = r.getBody().asString();
+//	System.out.println(body);
+//	}
+
 
     @Test
 	public void resteBasedataImsirelationRestAssuredTest()
 	{
    	System.out.println("1111111111111111111111111");
 
-	String myJson = "344930000000011";
-	RestAssured.baseURI  = "http://localhost:8080/TheBeanQuartet/rest/basedata/imsirelation";	
+	//Options choice = new Options ("VEA2", "2013-01-11 17:15:00","2013-01-11 17:18:00", null);
+	//String apiInput = "{\"VEA2\",\"2013-01-11 17:15:00\",\"2013-01-11 17:18:00\",\" \"}";
+	//String apiInput = "{\"option1\":\"VEA2\",\"option2\":\"2013-01-11 17:15:00\",\"option3\":\"2013-01-11 17:18:00\",\"option4\":\"\"}";
+	String apiInput = "{\"option1\":\"VEA2\",\"option2\":\"2013-01-11 17:15:00\",\"option3\":\"2013-01-11 17:18:00\",\"option4\":\"\"}";
 
-	Response r = given()
+	//String myJson1 = "VEA2";
+	//String myJson2 = "2013-01-11 17:15:00";
+	//String myJson3 = "2013-01-11 17:18:00";
+	RestAssured.baseURI  = "http://localhost:8080/TheBeanQuartet/rest/basedata/modelSearch";	
+	//RestAssured.authentication = basic("username", "password");
+			//Response r =
+			given().auth().none()
 	.contentType("application/json").
-	body(myJson).
+	body(apiInput).
+	//body(myJson2).
+	//body(myJson3).
     when().
-    post("");
+    post("").then().statusCode(500);;
+	//then().statusCode(200);
 	
 	System.out.println("2222222222222222222222");
-	String body = r.getBody().asString();
-	System.out.println(body);
-
-	}
-
-	
-
-//    @Test
-//	public void resteBasedataImsirelationRestAssuredTest()
-//	{
-//   	System.out.println("1111111111111111111111111");
-//
-//	Options choice = new Options ("VEA2", "2013-01-11 17:15:00","2013-01-11 17:18:00", null);
-//
-//	String myJson1 = "VEA2";
-//	String myJson2 = "2013-01-11 17:15:00";
-//	String myJson3 = "2013-01-11 17:18:00";
-//	RestAssured.baseURI  = "http://localhost:8080/TheBeanQuartet/rest/basedata/modelSearch";	
-//
-//	Response r = given()
-//	.contentType("application/json").
-//	body(myJson1).
-//	body(myJson2).
-//	body(myJson3).
-//    when().
-//    post("");
-//	
-//	System.out.println("2222222222222222222222");
 //	String body = r.getBody().asString();
 //	System.out.println(body);
-//
+
+	}
+//	@Test
+//	public void testQuery(){
+//		 RequestSpecification requestSpecification = with().parameters("firstName", "John", "lastName", "Doe");
+//		 ResponseSpecification responseSpecification = expect().body("greeting", equalTo("Greetings John Doe"));
+//		 given(requestSpecification, responseSpecification).get("/greet");
 //	}
     
-
 }
